b2e9594afc6950f723b7249a4de8c16664cb7568
Mcysneiros <msocys@gmail.com>
2021-10-21 17:58:07 -0300

    react native

---
+++ src/screens/home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { View, Text } from 'react-native';
+import { Styles } from './style';
+
+export function Home() {
+  return (
+    <View style={Styles.container}>
+      <Text>Olá react Native</Text>
+    </View>
+  );
+}
